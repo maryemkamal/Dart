663d702848082621cf55953ad734df7ad51ed282
maryemkamal <121868686+maryemkamal@users.noreply.github.com>
2025-07-18 22:59:37 +0300

    Add files via upload

--- OOP.dart
+++ OOP.dart
@@ -1,24 +1,60 @@
 class Person {
-  String name;
-  int age;
+  String _name;
+  int _age;
 
-  Person(this.name, this.age);
+  Person(this._name, this._age);
+
+  // Getter & Setter for name
+  String get name => _name;
+  set name(String newName) {
+    if (newName.isNotEmpty) {
+      _name = newName;
+    } else {
+      print("Name can't be empty.");
+    }
+  }
+
+  // Getter & Setter for age
+  int get age => _age;
+  set age(int newAge) {
+    if (newAge >= 18) {
+      _age = newAge;
+    } else {
+      print("Age must be 18 or older.");
+    }
+  }
 
   void displayInfo() {
-    print('Name: $name');
-    print('Age: $age');
+    print('Name: $_name');
+    print('Age: $_age');
   }
 }
 
 class Employee extends Person {
-  String position;
+  String _position;
+  static int _counter = 0;
 
-  Employee(String name, int age, this.position) : super(name, age);
+  Employee(String name, int age, this._position) : super(name, age) {
+    _counter++; 
+  }
+
+  String get position => _position;
+  set position(String newPosition) {
+    if (newPosition.isNotEmpty) {
+      _position = newPosition;
+    } else {
+      print("Position can't be empty.");
+    }
+  }
 
   @override
   void displayInfo() {
-    super.displayInfo(); 
-    print('Position: $position');
+    super.displayInfo();
+    print('Position: $_position');
+  }
+
+  static void showCounter() {
+    print("Count is: $_counter");
   }
 }
 
@@ -28,6 +64,15 @@ void main() {
 
   print('------------------');
 
-  Employee emp2 = Employee('Jannah', 22 , 'Designer');
+  Employee emp2 = Employee('Jannah', 22, 'Designer');
   emp2.displayInfo();
+
+  print('------------------');
+
+ 
+  emp2.name = ''; 
+  emp2.age = 16; 
+  emp2.position = ''; 
+
+  Employee.showCounter();
 }
